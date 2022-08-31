c96be01173f3475602851a0fbfd46e5f76c8a2f8
bhavik <bhavik19s2000@gmail.com>
2022-09-01 00:28:01 +0530

    problem solve

--- task/dump.cpp
+++ task/dump.cpp
@@ -7,43 +7,43 @@ class student
 {
     char name[20];
     int roll;
-    int mark[2];
-    static int total;
+    int mark[3];
+    int total = 0;
     int result;
 
 public:
     student();
     void get();
+    void maritlis(student[]);
 };
-
-int student::total;
 
 student::student()
 {
+
     cout << "enter student name" << endl;
     cin >> name;
     cout << "enter student roll" << endl;
     cin >> roll;
     cout << "enter mark of 3 subject" << endl;
-    for (int i = 0; i <= 2; i++)
+    for (int i = 0; i < 3; i++)
     {
         cin >> mark[i];
 
         total += mark[i];
     }
 
-    // for (int i = 0; i <= 2; i++)
-    // {
-    //     if (mark[i] > 28)
-    //     {
-    //         result =1;
-    //     }
-    //     else
-    //     {
-    //         result =0;
-    //         break;
-    //     }
-    // }
+    for (int i = 0; i < 3; i++)
+    {
+        if (mark[i] > 28)
+        {
+            result = 1;
+        }
+        else
+        {
+            result = 0;
+            break;
+        }
+    }
 }
 
 void student::get()
@@ -51,7 +51,7 @@ void student::get()
     cout << endl;
     cout << name << endl;
     cout << roll << endl;
-    for (int i = 0; i <= 2; i++)
+    for (int i = 0; i < 3; i++)
     {
         cout << mark[i] << endl;
     }
@@ -59,9 +59,26 @@ void student::get()
     cout << result << endl;
 }
 
+void student::maritlis(student a[])
+{
+
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            if (a[i].result == 1 && (a[i].total >= a[j].total))
+            {
+                a[i].get();
+            }
+        }
+    }
+}
+
 int main()
 {
-    student a[1];
+    student a[4];
+
+    a[0].maritlis(a);
 
     return 0;
 }
